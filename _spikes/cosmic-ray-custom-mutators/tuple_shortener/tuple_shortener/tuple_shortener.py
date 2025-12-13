be21966c30707c7a4636706ced3870d1fd006ab3
ratkovsky5 <125183333+ratkovsky5@users.noreply.github.com>
2025-12-13 14:55:34 +0100

    Adding custom mutant: tuple_shortener

---
+++ _spikes/cosmic-ray-custom-mutators/tuple_shortener/tuple_shortener/tuple_shortener.py
@@ -0,0 +1,30 @@
+from cosmic_ray.operators.operator import Operator
+from parso.python.tree import ReturnStmt, YieldExpr
+from parso import parse
+
+class ShortenTuple(Operator):
+    
+    def mutation_positions(self, node):
+        if isinstance(node, (ReturnStmt, YieldExpr)) and len(node.children) >= 2:
+            return_value = node.children[1]
+            if return_value.type == "atom" and len(return_value.children) == 3:
+                left_operator = return_value.children[0]
+                tuple_values = return_value.children[1]
+                right_operator = return_value.children[2]
+                if (
+                    left_operator.type== "operator" and left_operator.value == "(" 
+                    and tuple_values.type == "testlist_comp" and len(tuple_values.children) >= 2 
+                    and right_operator.type == "operator" and right_operator.value == ")"
+                ):
+                    yield (tuple_values.start_pos, tuple_values.end_pos)
+
+    def mutate(self, node, index):
+        tuple_values = node.children[1].children[1]
+        if len(tuple_values.children) == 3:
+            tuple_values.children = tuple_values.children[:-1]
+        else:
+            tuple_values.children = tuple_values.children[:-2]
+        return node
+    
+    def examples(self):
+        return [(parse('return (1, 2, 3)').children[0], parse('return (1, 2)').children[0])]
